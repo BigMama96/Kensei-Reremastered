87b0d38a23275ef218890cf6e6529359c2d5f038
Joanna <Chanjoanna331@gmail.com>
2024-12-06 09:59:01 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,33 @@
+console.log("Script Running.");
+document.addEventListener('DOMContentLoaded', function () {
+
+    const showCharactersBtn = document.querySelector('.button.is-halfwidth:nth-of-type(1)');
+    const showSushiBtn = document.querySelector('.button.is-halfwidth:nth-of-type(2)');
+    const showAllBtn = document.querySelector('.button.is-halfwidth:nth-of-type(3)');
+
+    const charactersSection = document.getElementById('characters');
+    const sushiSection = document.getElementById('sushi');
+    const allSection = document.getElementById('all');
+  
+ 
+
+    showCharactersBtn.addEventListener('click', function () {
+      charactersSection.style.display = 'block'; 
+      sushiSection.style.display = 'none';        
+    });
+  
+ 
+    showSushiBtn.addEventListener('click', function () {
+      sushiSection.style.display = 'block';     
+      charactersSection.style.display = 'none';
+    });
+ 
+  
+  showAllBtn.addEventListener('click', function () {
+    sushiSection.style.display = 'block';     
+    charactersSection.style.display = 'block';
+  });
+});
+
+
 
